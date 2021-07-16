7cbbf9312dfef9f98fb13316c0f628c4614bff8b
zFantasyDEV <davigboo@gmail.com>
2021-07-16 15:59:19 -0300

    Calculadora primeira versao

---
+++ script.js
@@ -0,0 +1,39 @@
+function calcular(tipo, valor) {
+
+	if (tipo === 'acao'){
+
+		if (valor === 'c') {
+			//limpar o visor da calculadora
+			document.getElementById('resultado').value = ''
+			alert('Calculadora limpada!') // cria uma dialog informando que o visor foi limpado
+		}
+
+		if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+
+			// mostra no visor o operador
+			document.getElementById('resultado').value += valor
+
+		}
+
+		if (valor === '=') {
+			
+			let valor_campo = eval(document.getElementById('resultado').value)
+			document.getElementById('resultado').value = valor_campo
+
+		}
+
+	} else if (tipo === 'numero'){
+
+		/*
+		let valor_campo = document.getElementById('resultado').value 
+		document.getElementById('resultado').value = valor_campo + valor
+		PODE SER FEITO DESSA FORMA!
+		*/
+
+		document.getElementById('resultado').value += valor //porém assim, dá o mesmo resultado com bem menos código
+
+	}
+
+	//console.log(tipo, valor)
+
+}
